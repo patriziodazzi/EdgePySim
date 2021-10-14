684cb92a0497178c3dcce5e2a06ff8e53362733c
Patrizio Dazzi <patrizio.dazzi@gmail.com>
2021-10-14 16:57:26 +0200

    improving orchestrator.py

--- edgepysim/orchestrator/orchestrator.py
+++ edgepysim/orchestrator/orchestrator.py
@@ -1,3 +1,4 @@
+from edgepysim.application import Microservice, Application
 from edgepysim.infrastructure import EdgeCluster, Cloud
 
 
@@ -10,12 +11,20 @@ class GlobalOrchestrator(Orchestrator):
     def __init__(self):
         super().__init__()
 
+    def deploy(self, application: Application) -> bool:
+        pass
+
 
 class CloudOrchestrator(Orchestrator):
     def __init__(self, cloud: Cloud):
         super().__init__()
 
+    def deploy(self, services: set[Microservice]) -> bool:
+        pass
 
 class EdgeOrchestrator(Orchestrator):
     def __init__(self, edge_cluster: EdgeCluster):
         super().__init__()
+
+    def deploy(self, services: set[Microservice]) -> bool:
+        pass
