9e3bfe13905991b294910b7e0fc5f603cd8a9231
Patrizio Dazzi <patrizio.dazzi@gmail.com>
2021-10-21 21:57:35 +0200

    Restructuring

--- src/edgepysim/__init__.py
+++ src/edgepysim/__init__.py
@@ -1,13 +1,13 @@
 # Forward declarations
 
-from edgepysim.base.definition import ResourceType, Orchestrator
-from edgepysim.resource.descriptor import ResourceDescriptor, IntegerResourceDescriptor, \
+from edgepysim.resource.descriptor import ResourceType, ResourceDescriptor, IntegerResourceDescriptor, \
     MemoryAmountResourceDescriptor, NetworkBandwidthResourceDescriptor, ProcessingCapacityResourceDescriptor, \
     StorageSpaceResourceDescriptor
 from edgepysim.resource.requirement import Requirement, RequirementSet, IntegerRequirement, \
     MemoryAmountRequirement, NetworkBandwidthRequirement, ProcessingCapacityResourceRequirement, StorageSpaceRequirement
 from edgepysim.application.application import Application, Image, Microservice, Volume
 from edgepysim.device.device import Device, CloudDevice, EdgeDevice
+from edgepysim.orchestrator.base import Orchestrator
 from edgepysim.infrastructure.cluster import Cloud, ComputingInfrastructure, EdgeCluster
 from edgepysim.orchestrator.orchestrator import GlobalOrchestrator, EdgeOrchestrator, CloudOrchestrator
 from edgepysim.infrastructure.continuum import ComputingContinuum
---
+++ src/edgepysim/orchestrator/base.py
@@ -0,0 +1,24 @@
+from edgepysim import *
+
+
+class Orchestrator(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def is_device_satisfying_requirement(dev: Device, req: Requirement) -> bool:
+        available_resources = dev.get_resources()
+
+        actual_resource = None
+        for i_rt, i_rd in available_resources.items():
+            if i_rt == req.rd.res_type:
+                actual_resource = i_rd
+
+        if not actual_resource:
+            return False
+
+        return req.is_satisfied_by_resource(actual_resource)
+
+    @staticmethod
+    def is_device_satisfying_all_requirements(dev: Device, rs: RequirementSet) -> bool:
+        return all(map(lambda r: Orchestrator.is_device_satisfying_requirement(dev, r), rs))
